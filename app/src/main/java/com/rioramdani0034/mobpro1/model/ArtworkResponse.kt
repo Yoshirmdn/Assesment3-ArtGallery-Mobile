3f9232d4171a03148398dd55fa10289ca36744d8
Rio Ragil Ramdani <rioragilramdani7@gmail.com>
2025-06-11 22:10:28 +0700

    Add ArtworkResponse and DeleteResponse data models for API responses

---
+++ app/src/main/java/com/rioramdani0034/mobpro1/model/ArtworkResponse.kt
@@ -0,0 +1,6 @@
+package com.rioramdani0034.mobpro1.model
+
+data class ArtworkResponse(
+    val message: String,
+    val data: List<Art>
+)
---
+++ app/src/main/java/com/rioramdani0034/mobpro1/model/DeleteResponse.kt
@@ -0,0 +1,6 @@
+package com.rioramdani0034.mobpro1.model
+
+data class DeleteResponse(
+    val message: String,
+    val removed: Art
+)
